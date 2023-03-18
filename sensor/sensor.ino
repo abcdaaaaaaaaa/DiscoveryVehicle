cbfaa8945b495ebfaac02709df91c4c16cdd2b22
abcdaaaaaaaaa <108553778+abcdaaaaaaaaa@users.noreply.github.com>
2023-03-18 23:00:14 +0300

    Update sensor.ino

--- sensor/sensor.ino
+++ sensor/sensor.ino
@@ -26,8 +26,8 @@ MQUnifiedsensor MQ4(Board, Voltage_Resolution, ADC_Bit_Resolution, Pin4, Type);
 MQUnifiedsensor MQ135(Board, Voltage_Resolution, ADC_Bit_Resolution, Pin135, Type);
 
 // Set your access point network credentials
-const char* ssid = "A.Mert iPhone'u";
-const char* password = "murti2000?!";
+const char* ssid = "***";
+const char* password = "***";
 
 WiFiServer server2(80);
 String header;
