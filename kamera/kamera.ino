7e40680f26cbc60adadea1c480171fcaff7889af
abcdaaaaaaaaa <108553778+abcdaaaaaaaaa@users.noreply.github.com>
2023-03-23 18:14:56 +0300

    Update kamera.ino

--- kamera/kamera.ino
+++ kamera/kamera.ino
@@ -7,30 +7,11 @@
 #include "soc/soc.h" //disable brownout problems
 #include "soc/rtc_cntl_reg.h"  //disable brownout problems
 #include "esp_http_server.h"
-
+#include "camera_index.h"
 //Replace with your network credentials
 const char* ssid = "****";
 const char* password = "****";
 
-#define PART_BOUNDARY "123456789000000000000987654321"
-
-#define PWDN_GPIO_NUM     32
-#define RESET_GPIO_NUM    -1
-#define XCLK_GPIO_NUM      0
-#define SIOD_GPIO_NUM     26
-#define SIOC_GPIO_NUM     27
-
-#define Y9_GPIO_NUM       35
-#define Y8_GPIO_NUM       34
-#define Y7_GPIO_NUM       13
-#define Y6_GPIO_NUM       12
-#define Y5_GPIO_NUM       4
-#define Y4_GPIO_NUM       2
-#define Y3_GPIO_NUM       15
-#define Y2_GPIO_NUM       14
-#define VSYNC_GPIO_NUM    25
-#define HREF_GPIO_NUM     23
-#define PCLK_GPIO_NUM     22
 
 static const char* _STREAM_CONTENT_TYPE = "multipart/x-mixed-replace;boundary=" PART_BOUNDARY;
 static const char* _STREAM_BOUNDARY = "\r\n--" PART_BOUNDARY "\r\n";
