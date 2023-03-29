8734e1198ca9ca5ab007c11a3d1bf53769f126b7
abcdaaaaaaaaa <108553778+abcdaaaaaaaaa@users.noreply.github.com>
2023-03-29 21:41:35 +0300

    Update 90y.ino

--- kamera/90y.ino
+++ kamera/90y.ino
@@ -1,7 +1,11 @@
 #include <WiFi.h>
 const char* ssid      = "Chernobyl";          
-const char* password  = NULL;          
+const char* password  = NULL;
+
 IPAddress local_IP(192, 168, 1, 2);
+IPAddress gateway(255, 255, 0, 0);
+IPAddress subnet(255, 255, 0, 0);
+
 void setup() {
   Serial.begin(115200); 
   Serial.setDebugOutput(true);                
@@ -9,6 +13,11 @@ void setup() {
 
   cameraInit();                              
 
+  WiFi.config(local_IP, gateway, subnet);
+if(!WiFi.config(local_IP, gateway, subnet)) {
+  Serial.println("STA Failed to configure");
+}
+  
   WiFi.begin(ssid, password);                 
 
   while (WiFi.status() != WL_CONNECTED)      
