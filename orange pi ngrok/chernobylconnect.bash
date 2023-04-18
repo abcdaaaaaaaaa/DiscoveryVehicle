f3af6f55cc02b9216841e3f55d93270b3edfafdd
abcdaaaaaaaaa <108553778+abcdaaaaaaaaa@users.noreply.github.com>
2023-04-18 20:50:39 +0300

    Update chernobylconnect.bash

--- orange pi ngrok/chernobylconnect.bash
+++ orange pi ngrok/chernobylconnect.bash
@@ -1,2 +1,2 @@
-./ngrok http http://192.168.1.1
-./ngrok http -auth="username:password" http://192.168.1.1
+./ngrok http http://192.168.1.2
+./ngrok http -auth="username:password" http://192.168.1.2
