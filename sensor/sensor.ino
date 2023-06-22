a23ab59b03decd5ca1d45ab3fad8975366b6236c
abcdaaaaaaaaa <108553778+abcdaaaaaaaaa@users.noreply.github.com>
2023-06-22 18:28:59 +0300

    Update sensor.ino

--- sensor/sensor.ino
+++ sensor/sensor.ino
@@ -1,17 +1,18 @@
 #include "WiFi.h"
-#include "ESPAsyncWebServer.h"
+#include "ThingSpeak.h"
 #include <MQUnifiedsensor.h>
-#include <HTTPClient.h>
+#include <TinyGPS++.h>
 
-IPAddress local_IP(192, 168, 1, 1);
-IPAddress gateway(255, 255, 0, 0);
-IPAddress subnet(255, 255, 0, 0);
 
 #define         Board                    ("ESP-32")
 #define         Pin2                      analogRead(A2)  
 #define         Pin3                      analogRead(A3)  
 #define         Pin4                      analogRead(A4)  
 #define         Pin135                    analogRead(A5)  
+#define         SpaceData                 analogRead(A1)
+#define         rxPin                     digitalRead(D0)
+#define         txPin                     digitalRead(D1)
+
 #define         RatioMQ2CleanAir          (9.83)
 #define         RatioMQ3CleanAir          (60) //RS / R0 = 60 ppm 
 #define         RatioMQ4CleanAir          (4.4) //RS / R0 = 4.4 ppm 
@@ -19,35 +20,37 @@ IPAddress subnet(255, 255, 0, 0);
 #define         ADC_Bit_Resolution        (13) // 10 bit ADC 
 #define         Voltage_Resolution        (3.3) // Volt resolution to calc the voltage
 #define         Type                      ("ESP-32") //Board used
-//Declare Sensor
+
 MQUnifiedsensor MQ2(Board, Voltage_Resolution, ADC_Bit_Resolution, Pin2, Type);
 MQUnifiedsensor MQ3(Board, Voltage_Resolution, ADC_Bit_Resolution, Pin3, Type);
 MQUnifiedsensor MQ4(Board, Voltage_Resolution, ADC_Bit_Resolution, Pin4, Type);
 MQUnifiedsensor MQ135(Board, Voltage_Resolution, ADC_Bit_Resolution, Pin135, Type);
 
-// Set your access point network credentials
-const char* ssid = "****";
-const char* password = "****";
-
-WiFiServer server2(80);
-String header;
-
-unsigned long counts; //variable for GM Tube events
-unsigned long previousMillis; //variable for measuring time
-float averageCPM;
-float sdCPM;
+unsigned long counts, previousMillis; 
+float averageCPM, sdCPM, calcCPM, CPMArray[100];
+String latitude, longitude;
 int currentCPM;
-float calcCPM;
-float CPMArray[100];
+
 #define LOG_PERIOD 30000
+HardwareSerial neogps(1);
+TinyGPSPlus gps;
 
-AsyncWebServer server(80);
+const char* ssid = "REPLACE_WITH_YOUR_SSID";  
+const char* password = "REPLACE_WITH_YOUR_PASSWORD";  
+WiFiClient  client;
 
-const char index_html[] PROGMEM = R"rawliteral(
-<!DOCTYPE HTML><html>
-<html><head></head><body><img src="192.168.1.2:81/stream"></body></html>
-)rawliteral";
-void setup(){
+unsigned long hello3 = 3;
+unsigned long hello2 = 2;
+unsigned long hello1 = 1;
+
+const char * myWriteAPIKey1 = "J2UEIZSZTC5568NM";
+const char * myWriteAPIKey2 = "ZVZ3UYIV4PTYA6XP";
+const char * myWriteAPIKey3 = "0HSEW6FQ65NWH2EP";
+
+unsigned long lastTime = 0;
+unsigned long timerDelay = 15000;
+
+void setup() {
   MQ2.init();
   MQ2.setRegressionMethod(1);
   MQ2.setR0(1.82);
@@ -67,48 +70,24 @@ void setup(){
   calcCPM = 0;
   pinMode(D8, INPUT);
   attachInterrupt(digitalPinToInterrupt(D8), impulse, FALLING);  
-  // Serial port for debugging purposes
-  Serial.begin(115200);
-  Serial.println();
-  
-WiFi.config(local_IP, gateway, subnet);
-if(!WiFi.config(local_IP, gateway, subnet)) {
-  Serial.println("STA Failed to configure");
-}  
-
+  Serial.begin(115200);  
+  WiFi.mode(WIFI_STA);  Serial.println("Connecting to WiFi ");
   WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED) {
+  while(WiFi.status() != WL_CONNECTED) { 
     delay(500);
     Serial.print(".");
   }
   Serial.println("");
-  Serial.println("WiFi connected");
-
-  Serial.print(WiFi.localIP());
-  Serial.println("' to connect");
-
-  server.on("/averagecpm", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send_P(200, "text/plain", String(averageCPM).c_str());
-  });
-  server.on("/sdcpm", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send_P(200, "text/plain", String(sdCPM).c_str());
-  });
-  server.on("/cpmarray", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send_P(200, "text/plain", String(CPMArray[currentCPM]).c_str());
-  });
-  server.on("/camera", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send_P(200, "text/plain", index_html);
-  });
-
-  // Start server
-  server.begin();
-  server2.begin();
-}
- 
-void loop(){
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
   
-radyoactivite();
+  ThingSpeak.begin(client);  
+
+}
+
+void loop() {
+
+ radyoactivite();
 
   MQ2.update();
   MQ3.update();
@@ -155,82 +134,34 @@ radyoactivite();
   MQ135.setA(34.668); MQ135.setB(-3.369); // Aceton
   float Aceton = MQ135.readSensor(); 
 
-  delay(100);
-  
-  WiFiClient client = server2.available();                                 
+  if ((millis() - lastTime) > timerDelay) {
 
-  if(client)                                                              
-  {                                                   
-    String currentLine = "";                                              
-    while(client.connected()) 
-    {                      
-      if(client.available())                                              
-      {                       
-        char c = client.read();                                           
-        Serial.write(c);                                                  
-        if (c == '\n')                                                    
-        {                                                                 
-          if(currentLine.length() == 0)                                   
-          {         
-client.println("HTTP/1.1 200 OK");
-client.println("Content-type:text/html");
-client.println("Connection: close");
-client.println();
-client.println("<!DOCTYPE html><html>");
-client.println("<html><head>");
-client.println("<meta charset='utf-8'>");
-client.println("<meta name='viewport' content='width=device-width,initial-scale=1'>");
-client.println("<link rel='shortcut icon' href='https://imgur.com/a/qodMVY0'>");
-client.println("<title>Chernobyl</title>");
-client.println("<style>");
-client.println("body {");
-client.println(" font-family: Arial,Helvetica,sans-serif;");
-client.println("background: #000e44;");
-client.println("color: #FFFFFF;");
-client.println("font-size: 20px;");
-client.println("font-weight: bold;");
-client.println("</style>");
-client.println("</head>");
-client.println("<body>"); 
-client.println("<h1></h1><table><tbody><tr><th></th><th></th></tr>");
-client.println("<tr><td> <font color='orange'>Alcohol:</font></td><td><span class=\"sensor\">");
-client.println(Alcohol);
-client.println("<tr><td><font color='orange'>Benzene:</td><td><span class=\"sensor\">");
-client.println(Benzene);
-client.println("<tr><td><font color='orange'>Hexane:</td><td><span class=\"sensor\">");
-client.println(Hexane);
-client.println("<tr><td><font color='magenta'>CH4:</td><td><span class=\"sensor\">");
-client.println(CH4);
-client.println("<tr><td><font color='magenta'>Smoke:</td><td><span class=\"sensor\">");
-client.println(smoke);
-client.println("<tr><td><font color='cyan'>CO2:</td><td><span class=\"sensor\">");
-client.println(CO2);
-client.println("<tr><td><font color='cyan'>Toluen:</td><td><span class=\"sensor\">");
-client.println(Toluen);
-client.println("<tr><td><font color='cyan'>NH4:</td><td><span class=\"sensor\">");
-client.println(NH4);
-client.println("<tr><td><font color='cyan'>Aceton:</td><td><span class=\"sensor\">");
-client.println(Aceton);
-client.println("<tr><td><font color='red'>LPG:</td><td><span class=\"sensor\">");
-client.println(LPG);
-client.println("<tr><td><font color='red'>Propane:</td><td><span class=\"sensor\">");
-client.println(Propane);
-client.println("<tr><td><font color='aquamarine'>CO:</td><td><span class=\"sensor\">");
-client.println(CO);
-client.println("<tr><td><font color='aquamarine'>H2:</td><td><span class=\"sensor\">");
-client.println(H2);
-client.println("<tr><td><font color='blue'>Air Quality:</td><td><span class=\"sensor\">");
-client.println(Pin135);
-client.println("<tr><td><font color='yellow'>Radioactivite:</td><td><span class=\"sensor\">");
-client.println("Avg: " + String(averageCPM) + " +/- " + String(sdCPM));
-client.println("ArrayVal: " + String(CPMArray[currentCPM]));
-client.println("</body></html>");
-delay(2110);
-break;
-          }
-          }
-      }
-    }
+    ThingSpeak.setField(1, Alcohol);
+    ThingSpeak.setField(2, Benzene);
+    ThingSpeak.setField(3, Hexane);
+    ThingSpeak.setField(4, CH4);
+    ThingSpeak.setField(5, smoke);
+    ThingSpeak.setField(6,  CO2);
+    ThingSpeak.setField(7, Toluen);
+    ThingSpeak.setField(8, NH4);
+  int a = ThingSpeak.writeFields(hello1, myWriteAPIKey1);
+    ThingSpeak.setField(1, Aceton);
+    ThingSpeak.setField(2, LPG);
+    ThingSpeak.setField(3, Propane);
+    ThingSpeak.setField(4, CO);
+    ThingSpeak.setField(5, H2);
+    ThingSpeak.setField(6, (Pin135 / 8,191));
+    ThingSpeak.setField(7, (SpaceData / 8,191));
+  int b = ThingSpeak.writeFields(hello2, myWriteAPIKey2);
+    ThingSpeak.setField(1, averageCPM);
+    ThingSpeak.setField(2, sdCPM);
+    ThingSpeak.setField(3, CPMArray[currentCPM]);
+    ThingSpeak.setField(4, latitude);
+    ThingSpeak.setField(5, longitude);
+  int c = ThingSpeak.writeFields(hello3, myWriteAPIKey3);
+
+ 
+    lastTime = millis();
   }
 }
 void impulse() {
@@ -266,24 +197,26 @@ unsigned long currentMillis = millis();
   }
 }
 
-String httpGETRequest(const char* serverName) {
-  WiFiClient client;
-  HTTPClient http;
-    
-  // Your Domain name with URL path or IP address with path
-  http.begin(client, serverName);
-  
-  // Send HTTP POST request
-  int httpResponseCode = http.GET();
-  
-  String payload = "--"; 
-  
-  if (httpResponseCode>0) {
-
-    payload = http.getString();
+int sendGpsToServer()
+{
+  boolean newData = false;
+  for (unsigned long start = millis(); millis() - start < 2000;){
+    while(neogps.available()){
+      if(gps.encode(neogps.read())){
+        if(gps.location.isValid() == 1){
+          newData = true;
+          break;
+        }
+      }
+    }
   }
-  // Free resources
-  http.end();
 
-  return payload;
+  if(true){
+    newData = false;
+     
+    latitude = String(gps.location.lat(), 6); 
+    longitude = String(gps.location.lng(), 6); 
+
+  }
+   
 }
