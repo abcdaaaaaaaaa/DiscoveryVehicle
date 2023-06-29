696d2ec9fdd742472ae516068b293547058d9fd5
abcdaaaaaaaaa <108553778+abcdaaaaaaaaa@users.noreply.github.com>
2023-06-29 13:32:34 +0300

    Add files via upload

---
+++ ip webcam/installngrok.bash
@@ -0,0 +1,8 @@
+apt update && apt upgrade
+pkg install wget
+wget https://(copy ngrok file or zip)
+ls
+tar xvzf ngrok-stable-linux-arm.tgz
+ls
+/.ngrok authtoken YOURAUTHTOKEN
+clear 
---
+++ ip webcam/ngrokwebcam.bash
@@ -0,0 +1,1 @@
+ngrok http http://192.168 (your ip)
