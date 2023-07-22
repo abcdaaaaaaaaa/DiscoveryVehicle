ef0b16610752c4da39605afb3db5a3eacff4ef17
abcdaaaaaaaaa <108553778+abcdaaaaaaaaa@users.noreply.github.com>
2023-07-22 13:15:10 +0300

    Update chat1MQ135.js

--- data.uzay.info/website/chat1MQ135.js
+++ data.uzay.info/website/chat1MQ135.js
@@ -1,1 +1,39 @@
+const ctx = document.getElementById('barchart').getContext('2d');
+const barchart = new Chart(ctx, {
+      type: 'bar',
+      data : {
+     labels: ['CO', 'Alcohol', 'CO2', 'Toluen', 'NH4', 'Aceton', 'Air'],
+      datasets: [{
+        label: 'Air Quality',
+        data: [toplam1, toplam2, toplam3, toplam4, toplam5, toplam6,  toplam7],
+        backgroundColor: [
+          'rgba(255, 26, 104, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(0, 0, 255, 0.2)',
+          'rgba(58, 51, 255, 0.2)',
+        ],
+        borderColor: [
+          'rgba(255, 26, 104, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(0, 0, 255, 1)',
+          'rgba(58, 51, 255, 1)',
+        ],
+        borderWidth: 1
+      }]
+	  },
 
+      options: {
+	  indexAxis: 'y',
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+	  });
