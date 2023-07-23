ee3faf683c4dc70242ab81077ba4ced9d4672a5a
abcdaaaaaaaaa <108553778+abcdaaaaaaaaa@users.noreply.github.com>
2023-07-23 19:36:44 +0300

    Update chart1MQ303A.js

--- data.uzay.info/website/chart1MQ303A.js
+++ data.uzay.info/website/chart1MQ303A.js
@@ -1,1 +1,33 @@
+const ctx = document.getElementById('barchart').getContext('2d');
+const barchart = new Chart(ctx, {
+      type: 'bar',
+      data : {
+     labels: ['Isobutane', 'Hydrogen', 'Ethanol',  'Air'],
+      datasets: [{
+        label: 'MQ-303A',
+        data: [toplam1, toplam2, toplam3, toplam4],
+        backgroundColor: [
+          'rgba(255, 26, 104, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+        ],
+        borderColor: [
+          'rgba(255, 26, 104, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(75, 192, 192, 1)',
+        ],
+        borderWidth: 1
+      }]
+	  },
 
+      options: {
+	  indexAxis: 'y',
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+	  });
