767db4839d35afeb462dba815c06c6e470e3f4de
abcdaaaaaaaaa <108553778+abcdaaaaaaaaa@users.noreply.github.com>
2023-08-03 16:27:47 +0300

    Update dataValue2.js

--- data.uzay.info/dataValue2.js
+++ data.uzay.info/dataValue2.js
@@ -1,6 +1,6 @@
         $(document).ready(function() {
             function getData() {
-                var url ="https://api.thingspeak.com/channels/2195036/fields/1,2,3,4,5,6,7,8.json?api_key=DG5Q1OBE0MW2GDTF&results=1";
+                var url ="https://api.thingspeak.com/channels/2195036/fields/1,2,3,4,5,6,7.json?api_key=DG5Q1OBE0MW2GDTF&results=1";
 
                 $.getJSON(url, function(data) {
                     var field1Values = [];
@@ -10,7 +10,6 @@
                     var field5Values = [];
                     var field6Values = [];	
                     var field7Values = [];	
-                    var field8Values = [];	
                     var timestamps = [];
 
                     $.each(data.feeds, function(index, feed) {
@@ -21,18 +20,16 @@
 						field5Values.push(feed.field5);
 						field6Values.push(feed.field6);
 						field7Values.push(feed.field7);
-						field8Values.push(feed.field8);
 						timestamps.push(feed.created_at);
 					});
         
  let toplam9 =  parseInt(field1Values);   
  let toplam10 = parseInt(field2Values);   
  let toplam11 = parseInt(field3Values);   
- let toplam12 = parseInt(field4Values)/(10**6);   
- let toplam13 = parseInt(field5Values)/(10**6);    
- let toplam14 = parseInt(field6Values);
- let toplam15 = parseInt(field7Values);
- let toplam16 = parseInt(field8Values);
+ let toplam12 = parseInt(field4Values);
+ let toplam13 = parseInt(field5Values);    
+ let toplam14 = parseInt(field6Values)/(10**6);   
+ let toplam15 = parseInt(field7Values)/(10**6);   
 
    
 document.cookie = "value9="+toplam9;
@@ -42,8 +39,6 @@ document.cookie = "value12="+toplam12;
 document.cookie = "value13="+toplam13;
 document.cookie = "value14="+toplam14;
 document.cookie = "value15="+toplam15;
-document.cookie = "value16="+toplam16;
-
 
 console.log(toplam9);
 console.log(toplam10);
@@ -52,7 +47,6 @@ console.log(toplam12);
 console.log(toplam13);
 console.log(toplam14);
 console.log(toplam15);
-console.log(toplam16);
                 });
 
 
