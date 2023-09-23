9c624c7694ea199242c74ee2b9870219e7136f88
abcdaaaaaaaaa <108553778+abcdaaaaaaaaa@users.noreply.github.com>
2023-09-23 18:33:15 +0300

    Update set_GeigerCounter_csv.py

--- datascience/csv/set_GeigerCounter_csv.py
+++ datascience/csv/set_GeigerCounter_csv.py
@@ -1,8 +1,10 @@
 import csv
 
 data_list = []
+data_number = 1
 
 while True:
+    print(f"Data {data_number}:")
     usvhr = input("usV/hr: ")
 
     data_entry = {
@@ -11,28 +13,30 @@ while True:
 
     data_list.append(data_entry)
 
-    choice = input("Do you want to (A)dd data or (W)rite data to a new file? (A/W): ")
-    
-    if choice.lower() == 'a':
-        mode = 'a'
-    elif choice.lower() == 'w':
-        mode = 'w'
-    else:
-        print("Invalid choice. Please enter 'A' or 'W'.")
-        continue
-
-    with open('GeigerCounter.csv', mode, newline='') as file:
-        fieldnames = ['usV/hr']
-        writer = csv.DictWriter(file, fieldnames=fieldnames)
-        
-        if mode == 'w':
-            writer.writeheader()
-        
-        for data_entry in data_list:
-            writer.writerow(data_entry)
-
-    print("Data has been successfully written to the CSV file")
-
     continue_input = input("Do you want to add more data? (Y/N): ")
     if continue_input.lower() != 'y':
         break
+
+    data_number += 1
+
+write_mode = input("Do you want to (A)dd data or (W)rite data to a new file? (A/W): ")
+
+if write_mode.lower() == 'a':
+    mode = 'a'
+elif write_mode.lower() == 'w':
+    mode = 'w'
+else:
+    print("Invalid choice. Using 'A' (add) mode by default.")
+    mode = 'a'
+
+with open('GeigerCounter.csv', mode, newline='') as file:
+    fieldnames = ['usV/hr']
+    writer = csv.DictWriter(file, fieldnames=fieldnames)
+
+    if mode == 'w':
+        writer.writeheader()
+
+    for data_entry in data_list:
+        writer.writerow(data_entry)
+
+print("Data has been successfully written to the CSV file")
