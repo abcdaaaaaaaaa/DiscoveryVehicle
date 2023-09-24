2f791bc7c08a714d4a524ea948c79f709e501888
abcdaaaaaaaaa <108553778+abcdaaaaaaaaa@users.noreply.github.com>
2023-09-24 09:35:58 +0300

    Update ESP32Camera.ino

--- ESP32Camera/ESP32Camera.ino
+++ ESP32Camera/ESP32Camera.ino
@@ -1,28 +1,96 @@
-#include <WiFi.h>
+#include "WiFi.h"
+#include "esp_camera.h"
 
-const char* ssidAP      = "ESP32Camera";      
-const char* passwordAP  = NULL;               
+const char* ssid = "VINNWiFi_80B6";  
+const char* password = "92030887";  
 
-IPAddress apip(192,168,1,1);
-IPAddress gateway(192,168,1,1);
-IPAddress subnet(255,255,255,0);
+IPAddress local_IP(192, 168, 1, 2);
+IPAddress gateway(255, 255, 0, 0);
+IPAddress subnet(255, 255, 0, 0);
+
+void cameraInit(void);
+void startCameraServer();
 
 void setup() {
-  Serial.begin(115200);                       
-  Serial.setDebugOutput(true);                
+  Serial.begin(115200);  
+  Serial.setDebugOutput(true);
   Serial.println();
-  
-  cameraInit();                               
 
-  WiFi.softAP(ssidAP, passwordAP);
-  WiFi.softAPConfig(apip, gateway, subnet);
-  delay(100);
+  cameraInit();  
 
-  void startCameraServer();                        
+  WiFi.begin(ssid, password);  
 
-  Serial.print("'http://");  
-  Serial.print(WiFi.softAPIP());                          
-  Serial.println("'Your Camera ip is ready!");      
-}       
+  while (WiFi.status() != WL_CONNECTED) {  
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("Wi-Fi Connected!");
 
-void loop() {}
+  startCameraServer();  
+  Serial.print("Camera is ready! to connect 'http://");  
+  Serial.print(WiFi.localIP());                           
+  Serial.println("' adresse");
+}
+
+void loop() {delay(1000);}
+
+void cameraInit(void) {
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = CAMD2;
+  config.pin_d1 = CAMD3;
+  config.pin_d2 = CAMD4;
+  config.pin_d3 = CAMD5;
+  config.pin_d4 = CAMD6;
+  config.pin_d5 = CAMD7;
+  config.pin_d6 = CAMD8;
+  config.pin_d7 = CAMD9;
+  config.pin_xclk = CAMXC;
+  config.pin_pclk = CAMPC;
+  config.pin_vsync = CAMV;
+  config.pin_href = CAMH;
+  config.pin_sscb_sda = CAMSD;
+  config.pin_sscb_scl = CAMSC;
+  config.pin_pwdn = -1;
+  config.pin_reset = -1;
+  config.xclk_freq_hz = 15000000;
+  config.frame_size = FRAMESIZE_UXGA;
+  config.pixel_format = PIXFORMAT_JPEG;
+  //config.pixel_format = PIXFORMAT_RGB565; // for face detection/recognition
+  config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
+  config.fb_location = CAMERA_FB_IN_PSRAM;
+  config.jpeg_quality = 12;
+  config.fb_count = 1;
+
+  //init with high specs to pre-allocate larger buffers                     for larger pre-allocated frame buffer.
+  if (config.pixel_format == PIXFORMAT_JPEG) {
+    if (psramFound()) {
+      config.jpeg_quality = 10;
+      config.fb_count = 2;
+      config.grab_mode = CAMERA_GRAB_LATEST;
+    } else {
+      // Limit the frame size when PSRAM is not available
+      config.frame_size = FRAMESIZE_SVGA;
+      config.fb_location = CAMERA_FB_IN_DRAM;
+    }
+  } else {
+    // Best option for face detection/recognition
+    config.frame_size = FRAMESIZE_240X240;
+#if CONFIG_IDF_TARGET_ESP32S3
+    config.fb_count = 2;
+#endif
+  }
+
+  // Camera init
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+
+  sensor_t* s = esp_camera_sensor_get();
+  // Drop down frame size for higher initial frame rate
+  s->set_framesize(s, FRAMESIZE_QVGA);
+}
