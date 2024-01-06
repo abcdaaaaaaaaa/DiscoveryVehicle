f73af87b2f660520b3e6b9b033058b6ec60e6966
abcdaaaaaaaaa <108553778+abcdaaaaaaaaa@users.noreply.github.com>
2024-01-06 11:49:19 +0300

    Update newlidar.php

--- data.uzay.info/newlidar.php
+++ data.uzay.info/newlidar.php
@@ -29,66 +29,47 @@
         $Flux150 =  $_COOKIE["green150"];
         $Flux180 =  $_COOKIE["green180"];
 
+function customMap($value, $fromLow, $fromHigh, $toLow, $toHigh) {
+    return ($value - $fromLow) * ($toHigh - $toLow) / ($fromHigh - $fromLow) + $toLow;
+}
+
 $originalMin = -10;
 $originalMax = 60;
-
-$newMin = 0;
-$newMax = 255;
-
-$normalizedtemp0 = ($temp0 - $originalMin) / ($originalMax - $originalMin);
-$Rtemp0 = $newMin + ($normalizedtemp0 * ($newMax - $newMin));
-$Btemp0 = $newMax - ($normalizedtemp0 * ($newMax - $newMin));
-
-$normalizedtemp30 = ($temp30 - $originalMin) / ($originalMax - $originalMin);
-$Rtemp30 = $newMin + ($normalizedtemp30 * ($newMax - $newMin));
-$Btemp30 = $newMax - ($normalizedtemp30 * ($newMax - $newMin));
-
-$normalizedtemp60 = ($temp60 - $originalMin) / ($originalMax - $originalMin);
-$Rtemp60 = $newMin + ($normalizedtemp60 * ($newMax - $newMin));
-$Btemp60 = $newMax - ($normalizedtemp60 * ($newMax - $newMin));
-
-$normalizedtemp90 = ($temp90 - $originalMin) / ($originalMax - $originalMin);
-$Rtemp90 = $newMin + ($normalizedtemp90 * ($newMax - $newMin));
-$Btemp90 = $newMax - ($normalizedtemp90 * ($newMax - $newMin));
-
-$normalizedtemp120 = ($temp120 - $originalMin) / ($originalMax - $originalMin);
-$Rtemp120 = $newMin + ($normalizedtemp120 * ($newMax - $newMin));
-$Btemp120 = $newMax - ($normalizedtemp120 * ($newMax - $newMin));
-
-$normalizedtemp150 = ($temp150 - $originalMin) / ($originalMax - $originalMin);
-$Rtemp150 = $newMin + ($normalizedtemp150 * ($newMax - $newMin));
-$Btemp150 = $newMax - ($normalizedtemp150 * ($newMax - $newMin));
-
-$normalizedtemp180 = ($temp180 - $originalMin) / ($originalMax - $originalMin);
-$Rtemp180 = $newMin + ($normalizedtemp180 * ($newMax - $newMin));
-$Btemp180 = $newMax - ($normalizedtemp180 * ($newMax - $newMin));
 
 $originalMin2 = 0;
 $originalMax2 = 65535;
 
-$newMin2 = 0;
-$newMax2 = 255;
+$newMin = 0;
+$newMax = 255;
 
-$normalizedGreen0 = ($Flux0 - $originalMin2) / ($originalMax2 - $originalMin2);
-$Green0 = $newMax2 - ($normalizedGreen0 * ($newMax2 - $newMin2));
+$Rtemp0 = customMap($temp0, $originalMin, $originalMax, $newMin, $newMax);
+$Btemp0 = customMap($temp0, $originalMin, $originalMax, $newMax, $newMin);
 
-$normalizedGreen30 = ($Flux30 - $originalMin2) / ($originalMax2 - $originalMin2);
-$Green30 = $newMax2 - ($normalizedGreen30 * ($newMax2 - $newMin2));
+$Rtemp30 = customMap($temp30, $originalMin, $originalMax, $newMin, $newMax);
+$Btemp30 = customMap($temp30, $originalMin, $originalMax, $newMax, $newMin);
 
-$normalizedGreen60 = ($Flux60 - $originalMin2) / ($originalMax2 - $originalMin2);
-$Green60 = $newMax2 - ($normalizedGreen60 * ($newMax2 - $newMin2));
+$Rtemp60 = customMap($temp60, $originalMin, $originalMax, $newMin, $newMax);
+$Btemp60 = customMap($temp60, $originalMin, $originalMax, $newMax, $newMin);
 
-$normalizedGreen90 = ($Flux90 - $originalMin2) / ($originalMax2 - $originalMin2);
-$Green90 = $newMax2 - ($normalizedGreen90 * ($newMax2 - $newMin2));
+$Rtemp90 = customMap($temp90, $originalMin, $originalMax, $newMin, $newMax);
+$Btemp90 = customMap($temp90, $originalMin, $originalMax, $newMax, $newMin);
 
-$normalizedGreen120 = ($Flux120 - $originalMin2) / ($originalMax2 - $originalMin2);
-$Green120 = $newMax2 - ($normalizedGreen120 * ($newMax2 - $newMin2));
+$Rtemp120 = customMap($temp120, $originalMin, $originalMax, $newMin, $newMax);
+$Btemp120 = customMap($temp120, $originalMin, $originalMax, $newMax, $newMin);
 
-$normalizedGreen150 = ($Flux150 - $originalMin2) / ($originalMax2 - $originalMin2);
-$Green150 = $newMax2 - ($normalizedGreen150 * ($newMax2 - $newMin2));
+$Rtemp150 = customMap($temp150, $originalMin, $originalMax, $newMin, $newMax);
+$Btemp150 = customMap($temp150, $originalMin, $originalMax, $newMax, $newMin);
 
-$normalizedGreen180 = ($Flux180 - $originalMin2) / ($originalMax2 - $originalMin2);
-$Green180 = $newMax2 - ($normalizedGreen180 * ($newMax2 - $newMin2));
+$Rtemp180 = customMap($temp180, $originalMin, $originalMax, $newMin, $newMax);
+$Btemp180 = customMap($temp180, $originalMin, $originalMax, $newMax, $newMin);
+
+$Green0   = customMap($Flux0, $originalMin2, $originalMax2, $newMax, $newMin);
+$Green30  = customMap($Flux30, $originalMin2, $originalMax2, $newMax, $newMin);
+$Green60  = customMap($Flux60, $originalMin2, $originalMax2, $newMax, $newMin);
+$Green90  = customMap($Flux90, $originalMin2, $originalMax2, $newMax, $newMin);
+$Green120 = customMap($Flux120, $originalMin2, $originalMax2, $newMax, $newMin);
+$Green150 = customMap($Flux150, $originalMin2, $originalMax2, $newMax, $newMin);
+$Green180 = customMap($Flux180, $originalMin2, $originalMax2, $newMax, $newMin);
 
 $rgb0 = "rgb(" . round($Rtemp0) . ", " . round($Green0) . ", " . round($Btemp0) . ")";
 $rgb30 = "rgb(" . round($Rtemp30) . ", " . round($Green30) . ", " . round($Btemp30) . ")";
