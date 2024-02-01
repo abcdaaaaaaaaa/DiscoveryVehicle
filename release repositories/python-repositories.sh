91f6a9c7e2cfc1794bcda4935d26f35484582946
abcdaaaaaaaaa <108553778+abcdaaaaaaaaa@users.noreply.github.com>
2024-02-02 00:48:50 +0300

    Update python-repositories.sh

--- release repositories/python-repositories.sh
+++ release repositories/python-repositories.sh
@@ -1,4 +1,4 @@
-python -m pip install opencv-python==4.7.0.72/4.1.1
+python -m pip install opencv-python==4.7.0.72 # 4.1.1
 python -m pip install pyautogui==0.9.54
 python -m pip install matplotlib==3.8.0
 python -m pip install csv==0.0.13
