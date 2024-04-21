8df5512572371d1221cacc34a7d77cc1a9662e15
abcdaaaaaaaaa <108553778+abcdaaaaaaaaa@users.noreply.github.com>
2024-04-21 22:29:54 +0300

    Update changing.py

--- Deep-Learning-Platform/changing.py
+++ Deep-Learning-Platform/changing.py
@@ -1,2 +1,4 @@
     parser.add_argument('--no-trace', action='store_false', help='don`t trace model')
-            for opt.weights in ['yolov7-e6e.pt']:
+
+    with torch.no_grad():
+        detect()
