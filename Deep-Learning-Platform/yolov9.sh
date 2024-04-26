69c73bb82cddbde9962663cb297d8dbbf0760ccf
abcdaaaaaaaaa <108553778+abcdaaaaaaaaa@users.noreply.github.com>
2024-04-26 23:12:40 +0300

    Delete Deep-Learning-Platform directory

--- Deep-Learning-Platform/yolov9.sh
+++
@@ -1,6 +0,0 @@
-git clone https://github.com/WongKinYiu/yolov9
-cd yolov9
-pip install -r requirements.txt
-python -m venv myenv
-myenv\Scripts\activate #For Windows
-source myenv/bin/activate #For Linux
