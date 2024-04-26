c8fc17b0942adc57cc3110e9564ac4cef6ed779f
abcdaaaaaaaaa <108553778+abcdaaaaaaaaa@users.noreply.github.com>
2024-04-26 23:37:54 +0300

    Update python-repositories.sh

--- release repositories/python-repositories.sh
+++ release repositories/python-repositories.sh
@@ -1,8 +1,8 @@
-python -m pip install opencv-python==4.7.0.72 # 4.1.1
+python -m pip install opencv-python==4.1.1
 python -m pip install pyautogui==0.9.54
-python -m pip install matplotlib==3.8.0
+python -m pip install matplotlib==3.8.3
 python -m pip install csv==0.0.13
 python -m pip install numpy==1.23.5
 python -m pip install scipy==1.10.1
 python -m pip install flask==3.0.0
-# For yolov7 installation, also see the deep learning platform's yolov7.sh file and requirements.txt
+# For yolov9 installation, also see the deep learning platform's yolov9.sh file and requirements.txt
