f7c9e10de30b3c327844093936abf021e6c2a602
abcdaaaaaaaaa <108553778+abcdaaaaaaaaa@users.noreply.github.com>
2024-05-25 17:39:46 +0300

    Update wokwi-lidar_values.ino

--- wokwi-simu-control/wokwi-lidar_values.ino
+++ wokwi-simu-control/wokwi-lidar_values.ino
@@ -33,34 +33,35 @@ void setup() {
 
 void loop() {
   if ((millis() - lastTime) > timerDelay) { 
-    Serial.begin("döngü başladı");
-    ThingSpeak.setField(1, String(1000)+String(10));
-    ThingSpeak.setField(2, String(2000)+String(20));
-    ThingSpeak.setField(3, String(3000)+String(30));
-    ThingSpeak.setField(4, String(4000)+String(40));
-    ThingSpeak.setField(5, String(5000)+String(50));
-    ThingSpeak.setField(6, String(6000)+String(60));
-    ThingSpeak.setField(7, String(7000)+String(70));
-    ThingSpeak.setField(8, String(8000)+String(80));
+    Serial.println("döngü başladı");
+    ThingSpeak.setField(1, String(1000 + 10000)+String(10 + 1000));
+    ThingSpeak.setField(2, String(2000 + 10000)+String(20 + 1000));
+    ThingSpeak.setField(3, String(3000 + 10000)+String(30 + 1000));
+    ThingSpeak.setField(4, String(4000 + 10000)+String(40 + 1000));
+    ThingSpeak.setField(5, String(5000 + 10000)+String(50 + 1000));
+    ThingSpeak.setField(6, String(6000 + 10000)+String(60 + 1000));
+    ThingSpeak.setField(7, String(7000 + 10000)+String(70 + 1000));
+    ThingSpeak.setField(8, String(8000 + 10000)+String(80 + 1000));
   int a = ThingSpeak.writeFields(hello1, myWriteAPIKey1);
-    ThingSpeak.setField(1, String(7500)+String(90));
-    ThingSpeak.setField(2, String(6500)+String(100));
-    ThingSpeak.setField(3, String(5500)+String(110));
-    ThingSpeak.setField(4, String(4500)+String(120));
-    ThingSpeak.setField(5, String(3500)+String(130));
-    ThingSpeak.setField(6, String(2500)+String(140));
-    ThingSpeak.setField(7, String(1500)+String(150));
-    ThingSpeak.setField(8, String(500)+String(160));
+    ThingSpeak.setField(1, String(7500 + 10000)+String(90 + 1000));
+    ThingSpeak.setField(2, String(6500 + 10000)+String(100 + 1000));
+    ThingSpeak.setField(3, String(5500 + 10000)+String(110 + 1000));
+    ThingSpeak.setField(4, String(4500 + 10000)+String(120 + 1000));
+    ThingSpeak.setField(5, String(3500 + 10000)+String(130 + 1000));
+    ThingSpeak.setField(6, String(2500 + 10000)+String(140 + 1000));
+    ThingSpeak.setField(7, String(1500 + 10000)+String(150 + 1000));
+    ThingSpeak.setField(8, String(500 + 10000)+String(160 + 1000));
   int b = ThingSpeak.writeFields(hello2, myWriteAPIKey2);
-    ThingSpeak.setField(1, String(250)+String(170));
-    ThingSpeak.setField(2, String(1250)+String(180));
-    ThingSpeak.setField(3, String(2250)+String(190));
-    ThingSpeak.setField(4, String(2350)+String(200));
-    ThingSpeak.setField(5, String(2450)+String(210));
-    ThingSpeak.setField(6, String(2550)+String(220));
-    ThingSpeak.setField(7, String(2650)+String(230));
-    ThingSpeak.setField(8, String(240)+String(250)+String(55));
+    ThingSpeak.setField(1, String(250 + 10000)+String(170 + 1000));
+    ThingSpeak.setField(2, String(1250 + 10000)+String(180 + 1000));
+    ThingSpeak.setField(3, String(2250 + 10000)+String(190 + 1000));
+    ThingSpeak.setField(4, String(3250 + 10000)+String(200 + 1000));
+    ThingSpeak.setField(5, String(4250 + 10000)+String(210 + 1000));
+    ThingSpeak.setField(6, String(5250 + 10000)+String(220 + 1000));
+    ThingSpeak.setField(7, String(6250 + 10000)+String(7250 + 10000));
+    ThingSpeak.setField(8, String(230 + 1000)+String(240 + 1000)+String(55 + 20));
   int c = ThingSpeak.writeFields(hello3, myWriteAPIKey3);
-    Serial.begin("döngü bitti");
+    Serial.println("döngü bitti");
+    lastTime = millis();
 }
 }
