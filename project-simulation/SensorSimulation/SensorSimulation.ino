3f0225a991d9bda20b32b19b9cc56742656c0634
abcdaaaaaaaaa <108553778+abcdaaaaaaaaa@users.noreply.github.com>
2025-07-28 19:22:49 +0300

    Update SensorSimulation.ino

--- project-simulation/SensorSimulation/SensorSimulation.ino
+++ project-simulation/SensorSimulation/SensorSimulation.ino
@@ -10,17 +10,17 @@
 #define GeigerPin  (39)
 #define analogPin  (35)
 #define gasPin     (34)
-#define DHTPIN     (33)
-#define potpin     (32)
+#define dhtPin     (33)
+#define potPin     (32)
 #define CLK        (18)
-#define DIO        (15)
+#define DIO        (5)
 
-TM1637 tm1637(CLK,DIO);
+TM1637 tm(CLK,DIO);
 
 HardwareSerial neogps(2); 
 TinyGPSPlus gps;
 
-DHT dht(DHTPIN, DHT22);
+DHT dht(dhtPin, DHT22);
 
 #define uSvhr1      (true)
 #define Avg1        (true)
@@ -31,7 +31,7 @@ DHT dht(DHTPIN, DHT22);
 GeigerCounter Radioactive(uSvhr1, Avg1, CPM_Count1, sdCPM1, LOG_PERIOD1, GeigerPin);
 
 int sec;
-String latitude, longitude, DhtValues;
+String latitude, longitude;
 
 const char* ssid = "Wokwi-GUEST";  
 const char* password = ""; 
@@ -52,12 +52,17 @@ unsigned long int timerDelay2 = 30000;
 
 void setup() {
   Serial.begin(115200);
-  tm1637.init();
-  tm1637.set(BRIGHT_TYPICAL);
-  pinMode(33, INPUT);
-  pinMode(34, INPUT);
+  tm.init();
+  tm.set(BRIGHT_TYPICAL);
+  tm.display(0, 1);
+  tm.display(1, 2);
+  tm.display(2, 3);
+  tm.display(3, 4);
+  dht.begin();
+  pinMode(potPin, INPUT);
+  pinMode(gasPin, INPUT);
+  pinMode(analogPin, INPUT);
   Radioactive.begin();
-  neogps.begin(9600, SERIAL_8N1, 16, 17); 
   WiFi.mode(WIFI_STA); Serial.println("Connecting to WiFi ");
   WiFi.begin(ssid, password);
   while(WiFi.status() != WL_CONNECTED) { 
@@ -72,96 +77,96 @@ void setup() {
 }
 
 void loop() {
-  sec = map(analogRead(32), 0, (1 << 12) - 1, 1, 19);
+  sec = map(analogRead(potPin), 0, (1 << 12) - 1, 1, 19);
 
   switch(sec){
   case (1):
   {
-  tm1637.display(3,5);
-  tm1637.display(2,3);
-  tm1637.display(1,1);
-  tm1637.display(0,0);
+  tm.display(3,5);
+  tm.display(2,3);
+  tm.display(1,1);
+  tm.display(0,0);
   }
   break;
   case 2: case 3: case 4: case 5: case 6: case 7: case 8: case 9: normal(); break;
   case (10):
   {
-  tm1637.display(3,1);
-  tm1637.display(2,3);
-  tm1637.display(1,1);
-  tm1637.display(0,0);
+  tm.display(3,1);
+  tm.display(2,3);
+  tm.display(1,1);
+  tm.display(0,0);
   }
   break;
   case (11):
   {
-  tm1637.display(3,1);
-  tm1637.display(2,3);
-  tm1637.display(1,1);
-  tm1637.display(0,1);
+  tm.display(3,1);
+  tm.display(2,3);
+  tm.display(1,1);
+  tm.display(0,1);
   }
   break;
   case (12):
   {
-  tm1637.display(3,6);
-  tm1637.display(2,3);
-  tm1637.display(1,1);
-  tm1637.display(0,0);
+  tm.display(3,6);
+  tm.display(2,3);
+  tm.display(1,1);
+  tm.display(0,0);
   }
   break;
   case (13):
   {
-  tm1637.display(3,7);
-  tm1637.display(2,3);
-  tm1637.display(1,1);
-  tm1637.display(0,0);
+  tm.display(3,7);
+  tm.display(2,3);
+  tm.display(1,1);
+  tm.display(0,0);
   }
   break;
   case (14):
   {
-  tm1637.display(3,8);
-  tm1637.display(2,3);
-  tm1637.display(1,1);
-  tm1637.display(0,0);
+  tm.display(3,8);
+  tm.display(2,3);
+  tm.display(1,1);
+  tm.display(0,0);
   }
   break;
   case (15):
   {
-  tm1637.display(3,4);
-  tm1637.display(2,1);
-  tm1637.display(1,2);
-  tm1637.display(0,0);
+  tm.display(3,4);
+  tm.display(2,1);
+  tm.display(1,2);
+  tm.display(0,0);
   }
   break;
   case (16):
   {
-  tm1637.display(3,10);
-  tm1637.display(2,3);
-  tm1637.display(1,0);
-  tm1637.display(0,3);
+  tm.display(3,10);
+  tm.display(2,3);
+  tm.display(1,0);
+  tm.display(0,3);
   }
   break;
   case (17):
   {
-  tm1637.display(3,11);
-  tm1637.display(2,3);
-  tm1637.display(1,0);
-  tm1637.display(0,3);
+  tm.display(3,11);
+  tm.display(2,3);
+  tm.display(1,0);
+  tm.display(0,3);
   }
   break;
   case (18):
   {
-  tm1637.display(3,10);
-  tm1637.display(2,7);
-  tm1637.display(1,0);
-  tm1637.display(0,3);
+  tm.display(3,10);
+  tm.display(2,7);
+  tm.display(1,0);
+  tm.display(0,3);
   }
   break;
   case (19):
   {
-  tm1637.display(3,10);
-  tm1637.display(2,9);
-  tm1637.display(1,0);
-  tm1637.display(0,3);
+  tm.display(3,10);
+  tm.display(2,9);
+  tm.display(1,0);
+  tm.display(0,3);
   }
   break;
   }
@@ -205,10 +210,10 @@ void loop() {
 }
 
 void normal(){
-tm1637.display(3,sec);
-tm1637.display(2,sec);
-tm1637.display(1,sec);
-tm1637.display(0,sec);
+tm.display(3,sec);
+tm.display(2,sec);
+tm.display(1,sec);
+tm.display(0,sec);
 }
 
 // DHT22 : [GND --> GND] [PIN --> D33] [VCC --> 5V]
@@ -216,5 +221,5 @@ tm1637.display(0,sec);
 // MQ-X: [PIN --> D35] [GND --> GND] [VCC --> 3.3V]
 // Other Sensor: [PIN --> D34] [GND --> GND] [VCC --> 3.3V]
 // Potentiometer: [PIN --> D32] [GND --> GND] [VCC --> 3.3V]
-// TM1637: [CLK --> D18] [DIO --> D15] [VCC --> 5V] [GND --> GND]
+// TM1637: [CLK --> D18] [DIO --> D5] [VCC --> 5V] [GND --> GND]
 // Geiger Counter: [GND --> GND] [PIN --> VN]
