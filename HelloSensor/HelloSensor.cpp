353d5ac05866efd70900fdb7f02c5ebb52031de2
abcdaaaaaaaaa <108553778+abcdaaaaaaaaa@users.noreply.github.com>
2026-06-15 22:17:21 +0300

    Update HelloSensor.cpp

--- HelloSensor/HelloSensor.cpp
+++ HelloSensor/HelloSensor.cpp
@@ -1,9 +1,8 @@
 #include "HelloSensor.h"
 
-GeigerCounter::GeigerCounter(int LOG_PERIOD1, byte pin2)
+GeigerCounter::GeigerCounter(byte pin2)
 {
   _pin2 = pin2;
-  _LOG_PERIOD = LOG_PERIOD1;
   _counts = 0;
   _currentCPM = 0;
   previousMillis = 0;
@@ -30,23 +29,21 @@ void GeigerCounter::radioactive() {
   _counts = interruptCounts;
   count = _counts;
 
-  unsigned long currentMillis = millis();
-  if (currentMillis - previousMillis > _LOG_PERIOD) {
-    previousMillis = currentMillis;
-    _CPMArray[_currentCPM] = _counts * 2;
-    usvhr = outputSieverts(_CPMArray[_currentCPM]);
+  previousMillis = currentMillis;
+  _CPMArray[_currentCPM] = _counts * 2;
+  usvhr = outputSieverts(_CPMArray[_currentCPM]);
 
-    interruptCounts = 0;
-    _averageCPM = 0; _sdCPM = 0;
+  interruptCounts = 0;
+  _averageCPM = 0; 
+  _sdCPM = 0;
 
-    for (int i = 0; i <= _currentCPM; i++) _averageCPM += _CPMArray[i];
-    _averageCPM /= (_currentCPM + 1);
+  for (int i = 0; i <= _currentCPM; i++) _averageCPM += _CPMArray[i];
+  _averageCPM /= (_currentCPM + 1);
 
-    for (int i = 0; i <= _currentCPM; i++) _sdCPM += sq(_CPMArray[i] - _averageCPM);
-    _sdCPM = sqrt(_sdCPM / _currentCPM) / sqrt(_currentCPM + 1);
+  for (int i = 0; i <= _currentCPM; i++) _sdCPM += sq(_CPMArray[i] - _averageCPM);
+  _sdCPM = sqrt(_sdCPM / _currentCPM) / sqrt(_currentCPM + 1);
 
-    Avg = outputSieverts(_averageCPM);
-    sdCPM = outputSieverts(_sdCPM);
-    _currentCPM++;
-  }
+  Avg = outputSieverts(_averageCPM);
+  sdCPM = outputSieverts(_sdCPM);
+  _currentCPM++;
 }
