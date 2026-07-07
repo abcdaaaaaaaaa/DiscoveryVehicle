eabefb55a36644eb570752443ca912316b6e04a7
abcdaaaaaaaaa <108553778+abcdaaaaaaaaa@users.noreply.github.com>
2026-07-07 19:14:51 +0300

    Update python-repositories.sh

--- release repositories/python-repositories.sh
+++ release repositories/python-repositories.sh
@@ -5,4 +5,5 @@ python -m pip install csv==0.0.13
 python -m pip install numpy==1.23.5
 python -m pip install scipy==1.10.1
 python -m pip install flask==3.0.0
+python -m pip install scikit-learn==1.4.2
 # For yolov9 installation, also see the deep learning platform's yolov9.sh file and requirements.txt
