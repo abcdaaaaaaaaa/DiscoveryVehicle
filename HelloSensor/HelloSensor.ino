9700c2e1771e7333cd8620dd87576c741da95814
abcdaaaaaaaaa <108553778+abcdaaaaaaaaa@users.noreply.github.com>
2026-07-07 20:39:11 +0300

    Update HelloSensor.ino

--- HelloSensor/HelloSensor.ino
+++ HelloSensor/HelloSensor.ino
@@ -3,43 +3,40 @@
 
 #include <WiFi.h>
 #include <DHT.h>
-#include <ThingSpeak.h>
+#include <HTTPClient.h>
+#include <NetworkClientSecure.h>
 #include <TM1637.h>
 #include "HelloSensor.h"
 #include <Deneyap_GPSveGLONASSkonumBelirleyici.h>
 
-#define ADC_BIT_RESU (12)
-#define GeigerPin    (39)
-#define analogPin    (35)
-#define gasPin       (34)
-#define dhtPin       (33)
-#define potPin       (32)
-#define CLK          (18)
-#define DIO          (5)
+#define GeigerPin  (39)
+#define analogPin  (35)
+#define gasPin     (34)
+#define dhtPin     (33)
+#define potPin     (32)
+#define CLK        (18)
+#define DIO        (5)
 
 GPS GPS;
 TM1637 tm(CLK,DIO);
 DHT dht(dhtPin, DHT22);
 
-GeigerCounter Radioactive(GeigerPin);
+GeigerCounter Radioactive(30000, GeigerPin);
 
 const char* ssid = "REPLACE_WITH_YOUR_SSID";  
 const char* password = "REPLACE_WITH_YOUR_PASSWORD";
-WiFiClient  client;
+const char* serverName = "https://air.uzay.info/datareceiver.php";
 
 int sec;
 
-unsigned long int hello1 = 1;
 static const char * myWriteAPIKey1 = "J2UEIZSZTC5568NM";
-
-unsigned long int hello2 = 2;
 static const char * myWriteAPIKey2 = "ZVZ3UYIV4PTYA6XP";
 
 unsigned long int lastTime1 = 0;
 unsigned long int timerDelay1 = 15000;
 
 unsigned long int lastTime2 = 0;
-unsigned long int timerDelay2 = 30000;
+unsigned long int timerDelay2 = 15000;
 
 void setup() {
   Serial.begin(115200);
@@ -68,13 +65,11 @@ void setup() {
   Serial.println("");
   Serial.print("Connected to WiFi network with IP Address: ");
   Serial.println(WiFi.localIP());
-  ThingSpeak.begin(client);
 }
 
 void loop() {
   Radioactive.radioactive();
-
-  sec = map(analogRead(potPin), 0, (1 << ADC_BIT_RESU) - 1, 1, 21);
+  sec = map(analogRead(potPin), 0, (1 << 12) - 1, 1, 21);
 
  switch(sec){
   case (1):
@@ -85,7 +80,8 @@ void loop() {
   tm.display(0,0);
   }
   break;
-  case 2: case 3: case 4: case 5: case 6: case 7: case 8: case 9: normal(); break;
+  case 2: case 3: case 4: case 5: case 6: case 7: case 8: case 9: normal();
+  break;
   case (10):
   {
   tm.display(3,1);
@@ -183,30 +179,51 @@ void loop() {
   }
 
   if ((millis() - lastTime1) > timerDelay1) {
-    ThingSpeak.setField(1, sec);
-    ThingSpeak.setField(2, analogRead(analogPin));
-    ThingSpeak.setField(3, (dht.readTemperature() + 140) * 10);
-    ThingSpeak.setField(4, (dht.readHumidity() + 100) * 10);
-    ThingSpeak.setField(5, analogRead(gasPin));
-
     GPS.readGPS(RMC);
     long lat = (long)((GPS.readLocationLat() + 90.0) * 10000000);
     long lng = (long)((GPS.readLocationLng() + 180.0) * 10000000);
 
-    ThingSpeak.setField(6, lat);
-    ThingSpeak.setField(7, lng);
-    int a = ThingSpeak.writeFields(hello1, myWriteAPIKey1);
+    String httpRequestData = "api_key=" + String(myWriteAPIKey1) + 
+                             "&field1=" + String(sec) + 
+                             "&field2=" + String(analogRead(analogPin)) + 
+                             "&field3=" + String((dht.readTemperature() + 140) * 10) + 
+                             "&field4=" + String((dht.readHumidity() + 100) * 10) + 
+                             "&field5=" + String(analogRead(gasPin)) + 
+                             "&field6=" + String(lat) + 
+                             "&field7=" + String(lng);
 
+    NetworkClientSecure *client = new NetworkClientSecure;
+    if(client) {
+        client->setInsecure();
+        HTTPClient http;
+        if (http.begin(*client, serverName)) {
+            http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+            int httpResponseCode = http.POST(httpRequestData);
+            http.end();
+        }
+        delete client;
+    }
     lastTime1 = millis();
   }
 
   if ((millis() - lastTime2) > timerDelay2) {
-    ThingSpeak.setField(1, (long)(Radioactive.usvhr * 10000));
-    ThingSpeak.setField(2, (long)(Radioactive.Avg * 10000));
-    ThingSpeak.setField(3, (long)(Radioactive.sdCPM * 10000));
-    ThingSpeak.setField(4, (long)(Radioactive.count));
-    int b = ThingSpeak.writeFields(hello2, myWriteAPIKey2);
+    String httpRequestData = "api_key=" + String(myWriteAPIKey2) + 
+                             "&field1=" + String((long)(Radioactive.usvhr * 10000)) + 
+                             "&field2=" + String((long)(Radioactive.Avg * 10000)) + 
+                             "&field3=" + String((long)(Radioactive.sdCPM * 10000)) + 
+                             "&field4=" + String((long)(Radioactive.count));
 
+    NetworkClientSecure *client = new NetworkClientSecure;
+    if(client) {
+        client->setInsecure();
+        HTTPClient http;
+        if (http.begin(*client, serverName)) {
+            http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+            int httpResponseCode = http.POST(httpRequestData);
+            http.end();
+        }
+        delete client;
+    }
     lastTime2 = millis();
   }
 }
@@ -221,6 +238,7 @@ tm.display(0,sec);
 // MQ-X: [PIN --> D35 (main-esp32), D35 (motor-control esp32)] [GND --> GND (main-esp32), GND (motor-control esp32)] [VCC --> 3.3V (main-esp32)]
 // Other Sensor: [PIN --> D34 (main-esp32), D34 (motor-control esp32)] [GND --> GND (main-esp32), GND (motor-control esp32)] [VCC --> 3.3V (main-esp32)]
 // Potentiometer: [PIN --> D32] [GND --> GND] [VCC --> 3.3V]
+
 // DHT22: [GND --> GND] [PIN --> D33] [VCC --> 3.3V]
 // GPS: [GND --> GND] [SCL --> D22] [SDA --> D21] [VCC --> 3.3V]
 // TM1637: [CLK --> D18] [DIO --> D5] [VCC --> 3.3V] [GND --> GND]
